61f3b40dd867f8292ea1565e9698d03db0ac4d88
saptarshi1992 <saptarshi34.sc@gmail.com>
2023-02-21 17:15:00 +0530

    class-3

---
+++ playground/1-one.js
@@ -0,0 +1,14 @@
+const fs = require ('fs')
+const jsondata =  {
+    name:"shon",
+    age:"40"
+}
+const josnwritedata  = JSON.stringify(jsondata)
+
+fs.writeFileSync('1-one.json',josnwritedata)
+
+
+/*const datajson = fs.readFileSync('1-one.json')
+const databuffer = datajson.toString()
+const data = JSON.parse(databuffer)
+console.log(data.name)*/
